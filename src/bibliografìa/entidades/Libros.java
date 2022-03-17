284520b9ef0edfe4947798661abd18ff47a343c0
Coloo <colouvillarecupero@gmail.com>
2022-03-16 21:10:10 -0300

    Segundo Commit

---
+++ src/bibliografìa/entidades/Libros.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bibliografìa.entidades;
+
+/**
+ *
+ * @author JORGE DANIEL UVILLA
+ */
+public class Libros {
+    private int numero_de_paginas;
+    private String autor;
+    
+    String te="s";
+            
+}
